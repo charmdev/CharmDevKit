aad921f3690a0f94a8c1e97aab92749d98e9185b
nulld <nulldivide@gmail.com>
2017-08-20 14:29:17 +0300

    remove extensions

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ HAXELIB='bin/haxe/haxelib'
 HAXELIB_ROOT='haxelib-root'
 HAXELIB_DEV='haxelib-dev'
 
-bin\vscode\bin\code --extensions-dir bin\vscode-extensions --install-extension nadako.vshaxe
-bin\vscode\bin\code --extensions-dir bin\vscode-extensions --install-extension msjsdiag.debugger-for-chrome
+bin/vscode/bin/code --extensions-dir bin/vscode-extensions --install-extension nadako.vshaxe
+bin/vscode/bin/code --extensions-dir bin/vscode-extensions --install-extension msjsdiag.debugger-for-chrome
 
 $HAXELIB setup $HAXELIB_ROOT
 
