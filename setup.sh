0f487bf5e80c4a9bd3c348d9a783520f0e5e3054
nulld <nulldivide@gmail.com>
2017-08-24 16:58:07 +0300

    deps changed

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ $HAXELIB setup $HAXELIB_ROOT
 
 $HAXELIB install standart_deps.hxml --always
 $HAXELIB dev openfl $HAXELIB_DEV/openfl
+$HAXELIB dev lime $HAXELIB_DEV/lime
 $HAXELIB dev repack $HAXELIB_DEV/repack/lib
 $HAXELIB dev samcodes-notifications $HAXELIB_DEV/samcodes-notifications
 $HAXELIB dev spinehaxe $HAXELIB_DEV/spinehaxe/src
-$HAXELIB dev stablexui $HAXELIB_DEV/stablexui
 $HAXELIB dev GAnalytics $HAXELIB_DEV/GAnalytics
 $HAXELIB dev extension-nativedialog $HAXELIB_DEV/extension-nativedialog
 $HAXELIB dev extension-iap $HAXELIB_DEV/extension-iap
