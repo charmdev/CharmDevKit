f7a9936ab14d045ffcd0aad12320385b4695a29d
nulld <nulldivide@gmail.com>
2017-08-24 15:56:35 +0300

    deps updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-HAXELIB='bin/haxe/haxelib'
+HAXELIB='bin/haxe/haxelib.exe'
 HAXELIB_ROOT='haxelib-root'
 HAXELIB_DEV='haxelib-dev'
 
@@ -6,5 +6,16 @@ $HAXELIB setup $HAXELIB_ROOT
 
 $HAXELIB install standart_deps.hxml --always
 $HAXELIB dev openfl $HAXELIB_DEV/openfl
+$HAXELIB dev repack $HAXELIB_DEV/repack/lib
+$HAXELIB dev samcodes-notifications $HAXELIB_DEV/samcodes-notifications
+$HAXELIB dev spinehaxe $HAXELIB_DEV/spinehaxe/src
+$HAXELIB dev stablexui $HAXELIB_DEV/stablexui
+$HAXELIB dev GAnalytics $HAXELIB_DEV/GAnalytics
+$HAXELIB dev extension-nativedialog $HAXELIB_DEV/extension-nativedialog
+$HAXELIB dev extension-iap $HAXELIB_DEV/extension-iap
+$HAXELIB dev extension-googleplaygames $HAXELIB_DEV/extension-googleplaygames
+$HAXELIB dev extension-facebook $HAXELIB_DEV/extension-facebook
+$HAXELIB dev ConnectionManagerExtension $HAXELIB_DEV/ConnectionManagerExtension
+$HAXELIB dev AppsFlyerExtension $HAXELIB_DEV/AppsFlyerExtension
 
  npm install http-server -g
